bef6a69ece2f60bf90d1aa8dae19083ffee6f8fa
FSDAWonder <fsd_a@wonderws.com>
2023-09-13 12:16:13 +0530

    Final Update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ const App = () => {
   const [pdfUrl, setPdfUrl] = useState("");
 
   const docs = [
-    { uri: require("./sample/sample.pdf"), fileName: "pdf1", fileType: "pdf" },
+    { uri: require("./sample/xpro.pdf"), fileName: "pdf", fileType: "pdf" },
+    // { uri: "https://docs.google.com/spreadsheets/d/1ylrv10lHZZRRqdLvo_5RTKFwYA8jsOtI_TkXGJxzeWI/edit#gid=0"},
+
+    // { uri: require("./sample/xpro.docx"), fileName: "doc1", fileType: "docx" },
   ];
 
   return (
     <div className="App">
-      <h1>Document Viewer</h1>
+      <h1 className="heading">Document Viewer</h1>
       <WaterMarker docs={docs[0].uri} setPdfUrl={setPdfUrl} />
      
       {pdfUrl ? (
@@ -23,6 +26,17 @@ const App = () => {
         <DocViewer
           pluginRenderers={DocViewerRenderers}
           documents={docs}
+          className="my-doc-viewer-style"
+          style={{width: 1350, height: 500}} 
+          theme={{
+            primary: "#5296d8",
+            secondary: "#ffffff",
+            tertiary: "#5296d899",
+            textPrimary: "#ffffff",
+            textSecondary: "#5296d8",
+            textTertiary: "#00000099",
+            disableThemeScrollbar: false,
+          }}
           prefetchMethod="GET"
           config={{
             header: {
--- src/Components/WaterMarker.js
+++ src/Components/WaterMarker.js
@@ -5,6 +5,8 @@ import UndoIcon from '@mui/icons-material/Undo';
 import RedoIcon from '@mui/icons-material/Redo';
 import Button from "@mui/material/Button";
 import Stack from '@mui/material/Stack';
+import CancelIcon from '@mui/icons-material/Cancel';
+
 const WaterMarker = ({ docs, setPdfUrl }) => {
   const [history, setHistory] = useState([]);
   const [historyIndex, setHistoryIndex] = useState(-1);
@@ -14,17 +16,20 @@ const WaterMarker = ({ docs, setPdfUrl }) => {
   const inputHandler = async (event) => {
     const targetFile = event.target.files[0];
     let reader = new FileReader();
-    reader.readAsArrayBuffer(targetFile);
-    reader.onload = function () {
-      setInputFile(reader.result);
-      const lengthHistory = history.length;
-      setHistoryIndex(lengthHistory);
-      setHistory((current) => [...current, reader.result]);
-      HandleWaterMarker(reader.result);
-    };
-    reader.onerror = function () {
-      console.log(reader.error);
-    };
+    if(targetFile){
+      reader.readAsArrayBuffer(targetFile);
+      reader.onload = function () {
+        setInputFile(reader.result);
+        const lengthHistory = history.length;
+        setHistoryIndex(lengthHistory);
+        setHistory((current) => [...current, reader.result]);
+        HandleWaterMarker(reader.result);
+      };
+      reader.onerror = function () {
+        console.log(reader.error);
+      };
+    }
+   
   };
 
   const HandleWaterMarker = async (imgFile) => {
@@ -47,6 +52,7 @@ const WaterMarker = ({ docs, setPdfUrl }) => {
         y: pages[i].getHeight() / 2 - pngDims.height + 150,
         width: pngDims.width,
         height: pngDims.height,
+        opacity:0.5,
       });
     }
     const pdfBytes = await pdfDoc.save();
@@ -73,11 +79,13 @@ const WaterMarker = ({ docs, setPdfUrl }) => {
       setPdfUrl("")
     }
   };
-
+const onCancel =() => {
+  setPdfUrl("")
+}
   return (
     <Stack direction="column">
     <div>
-      <Button startIcon={<CloudUploadIcon />} variant="contained" onClick={handleWatermarkPdf}>Add WaterMark</Button>
+      <Button sx={{ width: 180, padding: 1, margin: 1 }} startIcon={<CloudUploadIcon />} variant="contained" onClick={handleWatermarkPdf}>Add WaterMark</Button>
       <input
         onChange={inputHandler}
         ref={fileRef}
@@ -85,8 +93,10 @@ const WaterMarker = ({ docs, setPdfUrl }) => {
         type="file"
         accept="image/png"
       ></input>
-      <Button startIcon={<UndoIcon />}  onClick={() =>unDoHandler("prev")} variant="outlined">Undo</Button>
-      <Button startIcon={<RedoIcon />} onClick={() =>unDoHandler("next")} variant="outlined">Redo</Button>
+      <Button sx={{ width: 120, padding: 1, margin: 1 }} startIcon={<UndoIcon />} onClick={() =>unDoHandler("prev")} variant="outlined">Undo</Button>
+      <Button sx={{ width: 120, padding: 1, margin: 1 }} startIcon={<RedoIcon />} onClick={() =>unDoHandler("next")} variant="outlined">Redo</Button>
+      <CancelIcon className="cancel-icon" onClick={onCancel}/>
+      
     </div>
     </Stack>
   );
