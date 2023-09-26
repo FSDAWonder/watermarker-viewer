40ed89d4e42233406dd16fd2abbd226eb261d873
Venkatesan-Fsd-Wws <fsd@wonderws.com>
2023-09-26 19:19:31 +0530

    added unit test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import DocViewer from "@cyntler/react-doc-viewer";
 
-test('renders learn react link', () => {
+test('component render or not', () => {
   render(<App />);
   const linkElement = screen.getByText(/Document Viewer/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("component render with no documents", () => {
+  render(<DocViewer documents={[]} />);
+  const linkElement = screen.getByTestId("react-doc-viewer");
+  expect(linkElement).toMatchSnapshot();
 });
